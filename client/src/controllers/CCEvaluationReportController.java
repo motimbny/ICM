079d94630bc460017e58856d17ebf581c160af9a
gefenmargolin <57354869+gefenmargolin@users.noreply.github.com>
2019-12-22 21:02:33 +0200

    initControler

---
+++ client/src/controllers/CCEvaluationReportController.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CCEvaluationReportController {
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button showRequest;
+
+    @FXML
+    private Button personalInfo;
+
+    @FXML
+    private Button Help;
+
+    @FXML
+    private Button LogOut;
+
+    @FXML
+    private TextField Location;
+
+    @FXML
+    private TextArea descriptionOfChangeRequired;
+
+    @FXML
+    private TextArea resultOfChange;
+
+    @FXML
+    private TextField timeEstimated;
+
+    @FXML
+    private TextArea constraintsAndRisks;
+
+    @FXML
+    private Button requireMoreInfo;
+
+    @FXML
+    private Button DenyCCEvaluationReport;
+
+    @FXML
+    private Button ApproveCCEvaluationReport;
+
+    @FXML
+    private TextField requestID;
+
+}
+
---
+++ client/src/controllers/CCRequestMoreInfoController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CCRequestMoreInfoController {
+
+    @FXML
+    private Button home;
+
+    @FXML
+    private Button showRequests;
+
+    @FXML
+    private Button personalInfo;
+
+    @FXML
+    private Button help;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    private TextArea requiredInformation;
+
+    @FXML
+    private Button sendCCRequireMoreInfo;
+
+    @FXML
+    private TextField requestID;
+
+    @FXML
+    private TextField date;
+
+}
+
