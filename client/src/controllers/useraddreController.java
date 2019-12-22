8e308a733d2a5a8cfc0130ccd26bed3dbf96eca8
motimbny <motimbny@users.noreply.github.com>
2019-12-22 21:27:35 +0200

    ADD-addrecontroller

---
+++ client/src/controllers/useraddreController.java
@@ -0,0 +1,73 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+public class useraddreController 
+{
+    @FXML
+    private Button homeBTN;
+    @FXML
+    private Button addreBTN;
+    @FXML
+    private Button showreBTN;
+    @FXML
+    private Button personBTN;
+    @FXML
+    private Button helpBTN;
+    @FXML
+    private Button logoutBTN;
+    @FXML
+    private Button submitRequestUserAddReq;
+    @FXML
+    private TextArea desExtSit;
+    @FXML
+    private ChoiceBox<?> chooseInfoCom;
+    @FXML
+    private TextArea desReqCha;
+    @FXML
+    private TextArea expChaBen;
+    @FXML
+    private TextArea MorInfoAdd;
+    @FXML
+    private Button addAttachmentsUserAddReq;
+    @FXML
+    void backToHome(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void backToRequest(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void backTohelp(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void backToinfo(MouseEvent event)
+    {
+
+    }
+
+    @FXML
+    void backTorAddequest(MouseEvent event) 
+    {
+
+    }
+
+    @FXML
+    void logut(MouseEvent event)
+    {
+
+    }
+
+}
