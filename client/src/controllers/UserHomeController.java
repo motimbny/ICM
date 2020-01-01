73d18f92092755c4eeab04d36eb38a1aabebcfdc
moti <moti@10.0.0.28>
2020-01-01 12:10:30 +0200

    superviser again

--- client/src/controllers/UserHomeController.java
+++ client/src/controllers/UserHomeController.java
@@ -13,8 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
-
-
 public class UserHomeController implements Initializable
 {
     public UserHomeController()
