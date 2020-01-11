2ce833ab69df2e1e26bb0446e2d533bc7dcf95b5
moti <moti@10.0.0.11>
2020-01-11 16:32:54 +0200

    no more thread warning!!!!! יא חביבתי

--- client/src/controllers/MainAllControllers.java
+++ client/src/controllers/MainAllControllers.java
@@ -123,39 +123,86 @@ public class MainAllControllers
 	}
 	public void setHomeUserNum(int num)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((UserHomeController) WindowToShow.controller).setRequestNumber(num);
+		}});
 	}
 	public void setHomeITNum(int num)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITHomeController) WindowToShow.controller).setRequestNumber(num);
+		}});
 	}
 	public void goodRequeSend()
 	{
 		((UserAddRequestController) WindowToShow.controller).setOnSucsess();		
 	}
 	public void submitExtensionRequest(int num)
+	{Platform.runLater(new Runnable()
 	{
-		((ITExtensionRequestController) WindowToShow.controller).setOnSucsess(num);		
+	@Override
+	public void run() 
+	{
+		((ITExtensionRequestController) WindowToShow.controller).setOnSucsess(num);	
+	}});
 	}
 	public void submitEstimatedTime(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITHandleRequestController) WindowToShow.controller).setOnSucsess();		
+		}});
 	}
 	public void showUserReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((UserShowRequestsController) WindowToShow.controller).setTextInTable(list);
+		}});
 	}
 	public void showITReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITshowRequestsController) WindowToShow.controller).setTextInTable(list);
-	}
+		}});
+		}
 	public void getITjobInReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITHandleRequestController) WindowToShow.controller).setPane(list);
+		}});
 	}
 	public void getITReqStage(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITExtensionRequestController) WindowToShow.controller).setTextInFields(list);
+	}});
 	}
 	public void submitFailurReport(ArrayList<Object> list)
 	{
@@ -167,7 +214,13 @@ public class MainAllControllers
 	}
 	public void getITReqLocation(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITMeaningAssessmentEvaluationReportController) WindowToShow.controller).setTextInFields(list);
+		}});
 	}
 	public void submitEvaluationReport(ArrayList<Object> list)
 	{
@@ -179,115 +232,254 @@ public class MainAllControllers
 	}*/
 	public void showUserReqDetails(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((UserRequestDetailsController) WindowToShow.controller).setTextInFields(list);
-	}
+	}});
+		}
 	public void showITReqDetails(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITRequestDetailsController) WindowToShow.controller).setTextInFields(list);
+		}});
 	}
 	public void showUserSPReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((UserShowRequestsController) WindowToShow.controller).clearTable();
 		((UserShowRequestsController) WindowToShow.controller).setTextInTable(list);
+		}});
 	}
 	public void showITSPReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITshowRequestsController) WindowToShow.controller).clearTable();
 		((ITshowRequestsController) WindowToShow.controller).setTextInTable(list);
+		}});
 	}
 	public void setUser(User user)
 	{
 		this.user=user;
 	}
 	public void setHomeSupervisorNum(int num)
-	{
+	{		
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorHomeController) WindowToShow.controller).setRequestNumber(num);
+		}});
 	}
 	public void showSupervisorSPReq(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorShowRequestsController) WindowToShow.controller).clearTable();
 		((SupervisorShowRequestsController) WindowToShow.controller).setTextInTable(list);
+		}});
 	}
 	public void showManagerEmployeeList(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
         if(list.get(0) instanceof Integer)
         {
             ((ITManagerEmployeesManagmentController) WindowToShow.controller).setVisable();
              list.remove(0);
         }
         ((ITManagerEmployeesManagmentController) WindowToShow.controller).setTextInTable(list);
+		}});
 
 	}
 	public void showSuperviserRequestList(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorShowRequestsController) WindowToShow.controller).setTextInTable(list);
+		}});
 	}
 	public void showSuperviserExtensionRequest(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorExtensionRequestController) WindowToShow.controller).setToFields((extensionrequest) list.get(0));
-		
+		}});
 	}
 	public void SupervisorUpdateRequest(ArrayList<Object> list) {
 		
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorUpdateRequestController) WindowToShow.controller).setToFields(list);
-		
+		}});
 	}
 	public void getListOfIT(ArrayList<Object> arrayList) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorUpdateRequestController) WindowToShow.controller).setListOfIT(arrayList);
-	}
+	}});
+		}
+		
 	public void getListOfITCEO(ArrayList<Object> arrayList) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITHandleRequestController) WindowToShow.controller).setListOfIT(arrayList);
+		}});
 	}
 	public void saveTesterSuccssesful(ArrayList<Object> arrayList) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITHandleRequestController) WindowToShow.controller).setOnSucsess();
+		}});
 	}
-	public void showITEvaluationReport(ArrayList<Object> arrayList) {
+	public void showITEvaluationReport(ArrayList<Object> arrayList) 
+	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITCCCEvaluationReportController) WindowToShow.controller).setTextInFields((Evluationreport) arrayList.get(0));
+		}});
 	}
 	public void ReqTimeSupervisor(ArrayList<Object> list) throws IOException
 	{
+		
 		((SupervisorShowRequestsController) WindowToShow.controller).setTimeWindow(list);		
 	}
 	public void SupervisorRequestDetailes(ArrayList<Object> list)
 	{
-		((SupervisorRequestDetailsController) WindowToShow.controller).setTextInFields(list);		
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
+		((SupervisorRequestDetailsController) WindowToShow.controller).setTextInFields(list);	
+		}});
 	}
 	public void ITManagerRequestDetailes(ArrayList<Object> list)
 	{
-		((ITManagerRequestDetailsController) WindowToShow.controller).setTextInFields(list);		
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
+		((ITManagerRequestDetailsController) WindowToShow.controller).setTextInFields(list);	
+		}});
 	}
 	public void SupervisorTimeRequest(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorTimeRequestController) WindowToShow.controller).setToFields(list);
+		}});
 	}
 	public void SupervisorSaveTimeEv(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorTimeRequestController) WindowToShow.controller).setOnSucsess();
+		}});
 	}
 	public void showSuperviserExtensionRequestAnswer(ArrayList<Object> send) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorExtensionRequestController) WindowToShow.controller).showAnswer();
+		}});
 	}
 	public void setHomeMangerNum(int num) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITManagerHomeController) WindowToShow.controller).setRequestNumber(num);
+		}});
 	}
 	public void showMangerRequestList(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITManagerShowRequestsController)WindowToShow.controller).setTextTable(list);
+		}});
 	}
 	public void showManSPReq(ArrayList<Object> list) 
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITManagerShowRequestsController) WindowToShow.controller).clearTable();
 		((ITManagerShowRequestsController) WindowToShow.controller).setTextTable(list);
+		}});
 	}
 
 	public void ManagerShowMessage(ArrayList<Object> list) 
 		{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 			((ITManagerMessagesController)WindowToShow.controller).setTextTable(list);
+		}});
 		}
 
 /*	public void addTimeEstimated(Object object) {
@@ -301,25 +493,46 @@ public class MainAllControllers
 	}
 	public void SuperviserShowMessage(ArrayList<Object> list)
 	{
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((SupervisorMessagesController)WindowToShow.controller).setTextTable(list);	
+		}});
 	}
 	public void UserConnected() 
 	{
 		((LoginController) WindowToShow.controller).setVisibleLblCon();
 	}
-	public void makeActiveSuClo(ArrayList<Object> arrayList) {
+	public void makeActiveSuClo(ArrayList<Object> arrayList) 
+	{Platform.runLater(new Runnable()
+	{
+	@Override
+	public void run() 
+	{
 		
 		((ITManagerReportsController) WindowToShow.controller).setActiveSuClo(arrayList);
-
+	}});
 		
 	}
 	public void makePerformenct(ArrayList<Object> send) {
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
 		((ITManagerReportsController) WindowToShow.controller).setmakePerformenct(send);
-		
+		}});
 	}
 	public void makemakeDelays(ArrayList<Object> send) {
-	
-		((ITManagerReportsController) WindowToShow.controller).setmakeDelays(send);		
+		Platform.runLater(new Runnable()
+		{
+		@Override
+		public void run() 
+		{
+		((ITManagerReportsController) WindowToShow.controller).setmakeDelays(send);	
+		}});
 	}
 	
 }
