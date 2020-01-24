23bfcddcde151f4bc25f5ba4df8b28f0e880239d
adioren2408 <57354858+adioren2408@users.noreply.github.com>
2020-01-24 08:32:44 +0200

    cssads

--- client/src/unitTests/calculationTest.java
+++ client/src/unitTests/calculationTest.java
@@ -27,17 +27,17 @@ class calculationTest {
 	public void testevenlenth()
 	{
 		calculatwithoutDB.success.clear();
-		report f1=new report("2020-01-10","2020-01-22",0);
-		report f2=new report("2020-01-10","2020-01-22",1);
-		report f3=new report("2020-01-10","2020-01-22",2);
-		report f4=new report("2020-01-10","2020-01-22",3);
-		report f5=new report("2020-01-10","2020-01-22",4);
+		report f1=new report("2027-01-10","2028-01-22",0);
+		report f2=new report("2027-01-10","2028-01-22",1);
+		report f3=new report("2027-01-10","2028-01-22",2);
+		report f4=new report("2027-01-10","2028-01-22",3);
+		report f5=new report("2027-01-10","2028-01-22",4);
 		calculatwithoutDB.success.add(f1);
 		calculatwithoutDB.success.add(f2);
 		calculatwithoutDB.success.add(f3);
 		calculatwithoutDB.success.add(f4);
 		calculatwithoutDB.success.add(f5);
-		ArrayList<Object> get=universalScoring.makeActiveSuClo("2020-01-10","2020-01-22");
+		ArrayList<Object> get=universalScoring.makeActiveSuClo("2027-01-10","2029-01-22");
 		Object med=get.get(1);
 		Object dev=get.get(0);
 		assertEquals(2,med);
@@ -46,15 +46,15 @@ class calculationTest {
 	@Test
 	public void testoddlenth()
 	{		calculatwithoutDB.success.clear();
-	report f1=new report("2020-01-10","2020-01-22",0);
-	report f2=new report("2020-01-10","2020-01-22",1);
-	report f3=new report("2020-01-10","2020-01-22",2);
-	report f4=new report("2020-01-10","2020-01-22",3);
+	report f1=new report("2026-01-10","2026-01-22",0);
+	report f2=new report("2026-01-10","2026-01-22",1);
+	report f3=new report("2026-01-10","2026-01-22",2);
+	report f4=new report("2026-01-10","2026-01-22",3);
 	calculatwithoutDB.success.add(f1);
 	calculatwithoutDB.success.add(f2);
 	calculatwithoutDB.success.add(f3);
 	calculatwithoutDB.success.add(f4);
-		ArrayList<Object> get=universalScoring.makeActiveSuClo("2020-01-10","2020-01-22");
+		ArrayList<Object> get=universalScoring.makeActiveSuClo("2026-01-10","2026-01-22");
 		Object med=get.get(1);
 		Object dev=get.get(0);
 		assertEquals(2,med);
@@ -65,9 +65,9 @@ class calculationTest {
 	public void testOneReq()
 	{
 		calculatwithoutDB.success.clear();
-		report f4=new report("2020-01-10","2020-01-22",3);
+		report f4=new report("2028-01-10","2029-01-22",3);
 		calculatwithoutDB.success.add(f4);
-		ArrayList<Object> get=universalScoring.makeActiveSuClo("2020-01-10","2020-01-22");
+		ArrayList<Object> get=universalScoring.makeActiveSuClo("2028-01-10","2029-01-22");
 		Object med=get.get(1);
 		Object dev=get.get(0);
 		assertEquals(3,med);
